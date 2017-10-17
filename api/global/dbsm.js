55eca3cb15bc51fd39c4e3cf387fb633c3ba205a
Unknown <javad.hatami@sigma.se>
2017-10-17 12:28:26 +0200

    cg

--- api/global/dbsm.js
+++ api/global/dbsm.js
@@ -7,11 +7,9 @@ module.exports = {
   DBNoSql:'',
 	server:'',
 	validators:[],
-	schemas:[],
+	endpoints:[],
 	helpers:require('./helper'),
 	loadSchemasSql:function(cb,obj){
-
-
 			var self = this;
 			var models = [];
 			var result = [];
@@ -76,161 +74,68 @@ module.exports = {
 				else {
 					models[tplgy[title]] = {title:title,model:model};
 				}
-
+				var endpoints = ['/api/'+title, '/api/'+title+'/:id',propnames];
+				self.endpoints.push(endpoints);
 			}
-
-
-			function mark(marked)
-			{
-				var found = 0;
-				for(var y in model)
-				{
-					 var type = model[y].type;
-					 if(type == 'string')
-					 {
-						 type = 'varchar(255)';
-					 }
-					 if(type == 'text')
-					 {
-						 type= 'text';
-					 }
-					 if(type == 'int')
-					 {
-						 type = 'int(11)';
-					 }
-					 if(type == 'Bigint')
-					 {
-							type = 'bigint(20)';
-					 }
-					 if(type == 'float')
-					 {
-						 type = 'float(8,2)';
-					 }
-					 if(type == 'boolean')
-					 {
-						 type = 'tinyint(1)';
-					 }
-					if(y == name)
-					{
-						if(datatType  == type)
-						{
-							console.log(name);
-						}
-					}
-				}
-			}
-			console.log('parents');
-
 			var createTable = function (index){
-
-
 
 				if(models[index])
 				{
 					var model = models[index].model;
 					var title = models[index].title;
-
 					console.log('Check Table > '+title + ' index' +index);
 					self.DBSql.schema.hasTable(title).then(function(exists) {
 						if (!exists) {
 							console.log('create Table > '+title);
-							self.DBSql.schema.createTable(title, function(table) {
+							 self.DBSql.schema.createTable(title, function(table) {
 							 table.increments('id').primary();
 							 for(var y  in model)
 							 {
-									 var type = model[y].type;
-									 var value = model[y].defaultValue;
-									 if(type == 'string')
-									 {
-											 table.string(y, 255).defaultTo(value);
-									 }
-									 if(type == 'text')
-									 {
-											 table.text(y).defaultTo(value);;
-									 }
-									 if(type == 'int')
-									 {
-											 table.integer(y).defaultTo(value);;
-									 }
-									 if(type == 'Bigint')
-									 {
-											 table.bigInteger(y).defaultTo(value);;
-									 }
-									 if(type == 'float')
-									 {
-											 table.float(y).defaultTo(value);;
-									 }
-									 if(type == 'boolean')
-									 {
-											 table.boolean(y).defaultTo(value);;
-									 }
+								 var type = model[y].type;
+								 var value = model[y].defaultValue;
+								 if(type == 'string')
+								 {
+										 table.string(y, 255).defaultTo(value);
+								 }
+								 if(type == 'text')
+								 {
+										 table.text(y).defaultTo(value);;
+								 }
+								 if(type == 'int')
+								 {
+										 table.integer(y).defaultTo(value);;
+								 }
+								 if(type == 'Bigint')
+								 {
+										 table.bigInteger(y).defaultTo(value);;
+								 }
+								 if(type == 'float')
+								 {
+										 table.float(y).defaultTo(value);;
+								 }
+								 if(type == 'boolean')
+								 {
+										 table.boolean(y).defaultTo(value);;
+								 }
+								 if(type == 'datetime'){
+									table.timestamp(y).defaultTo(value)
+								 }
 							}
 							 if(parents[title])
 							 {
-								 console.log(parents[title]);
 								 for(var z in parents[title])
 								 {
 									 var children = parents[title][z];
 									 table.integer(children+'_id').unsigned().references('id').inTable(children);
 								 }
 							 }
-
 							 table.timestamps();
 						 }).then(function(resp) {
-
 							 index--;
 							 return createTable(index);
 						 });
-					 }else {
-						console.log('alter Table > '+title);
-
-  				var statement = "SELECT COLUMN_NAME,EXTRA,IS_NULLABLE,COLUMN_KEY,COLUMN_DEFAULT,DATA_TYPE,COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '"+Dbconfig.Dbname+"' AND TABLE_NAME = '"+title+"'";
-
-					console.log(statement);
-						self.DBSql.schema.raw(statement).then(function(resp) {
-
-							var myresult=JSON.stringify(resp);
-
-							var json =  JSON.parse(myresult);
-							var cols = json[0];
-							//console.log('>> string: ', cols );
-							var mark = [];
-
-							for(var x in cols)
-							{
-									var name =  cols[x].COLUMN_NAME;
-									var key =  cols[x].COLUMN_KEY;
-									var nullable =  cols[x].IS_NULLABLE;
-									var defaultValue =  cols[x].COLUMN_DEFAULT;
-									var datatType =  cols[x].DATA_TYPE;
-									var type =  cols[x].COLUMN_TYPE;
-								 if(key == 'MUL')
-								 {
-
-									 }
-									 else {
-
-									 }
-
-
-									 //forgienkey
-								 }
-								 else {
-
-								 }
-
-
-							}
-
-
-
-						 });
-
-						var query = self.DBSql.schema.alterTable(title, function(t) {
-
-						}).toString();
-
-						console.log(query)
+					 } else {
+						 console.log('Table existed > '+title);
 						index--;
 						return createTable(index);
 					 }
@@ -243,10 +148,7 @@ module.exports = {
 				}
 
 			}
-
-
 			createTable(level);
-			var endpoints = ['/api/'+title, '/api/'+title+'/:id',propnames];
 			cb();
 	},
 	loadSchemasNosql:function()
